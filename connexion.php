80e3cbe72d21b496f2817e2472b966781013643a
idrisse-mze-hamadi <91784605+idrisse-mze-hamadi@users.noreply.github.com>
2021-12-02 09:54:04 +0100

    modif adresse de la base de donné

--- connexion.php
+++ connexion.php
@@ -17,7 +17,7 @@ if(isset($_SESSION["login"]))
 {
     header("location:index.php"); //permet de rediriger vers la page d'accueil une fois connecté.
 }
-$bdd = mysqli_connect("localhost", "root", "", "moduleconnexion");
+$bdd = mysqli_connect("localhost:3306", "mze-idrisse", "******", "idrisse-mze-hamadi_module-connexion");
 
 if (isset($_POST["connexion"])) // lorsqu'on ce connecte
 {
